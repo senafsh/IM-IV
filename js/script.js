f64a66f7781b930bb6861c29ef9bc424442b8cd0
senafsh <selina.fischer@stud.fhgr.ch>
2024-05-14 15:11:46 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,122 +1,162 @@
 let url = 'https://197272-7.web.fhgr.ch/php/unload.php';
-let data;
+let parkhausDaten;
+
+let doughnutChart, lineChart;  // Globale Variablen für die Diagramme
+let lineChartArray = [];
+
+let wochenTagAuswahl = document.getElementById('wochentagAuswahl');
+
 
 // Funktion holt Daten von der API und wandelt sie in JSON um
 async function fetchData(url) {
     try {
         let response = await fetch(url);
-        data = await response.json();
+        let data = await response.json();
         return data;
     }
     catch (error) {
-        console.log(error);
+        // console.log(error);
         return {};
     }
 }
 
 // Initialisierung: Daten werden geladen
 async function init() {
+   // Leaflet-Karte
+   var map = L.map('map').setView([47.5596, 7.5886], 13);
+   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+       maxZoom: 19,
+       minZoom: 8,
+       attribution: '© OpenStreetMap'
+   }).addTo(map);
+
     let response = await fetchData(url);
-    data = response;
+    parkhausDaten = response;
+    // console.log(typeof parkhausDaten);
+    // console.log(parkhausDaten);
+    let heuteee = new Date();
+    let wti = heuteee.getDay();
+    wochenTagAuswahl.value = wti;
+    
 
     // Suchen Sie nach dem neuesten Eintrag für 'Parkhaus City'
-    const parkhausCityDaten = data['Parkhaus City'];
-    const neuesteDaten = parkhausCityDaten[parkhausCityDaten.length - 1];
+    //const parkhausCityDaten = parkhausDaten['Parkhaus City'];
 
-    const free_spaces = neuesteDaten['free_spaces'];
-    const total_spaces = neuesteDaten['total_spaces'];
-    const occupied_spaces = total_spaces - free_spaces;
+    for (const [key, value] of Object.entries(parkhausDaten)) {
+        
+        // console.log(key);
+        const parkhausCityDaten = value;
+        // console.log(parkhausCityDaten);
 
-    console.log('Freie Plätze:', free_spaces);
-    console.log('Besetzte Plätze:', occupied_spaces);
+        const neuesteDaten = parkhausCityDaten[parkhausCityDaten.length - 1];
 
-    // Doughnut-Diagramm erstellen
-    const belegungBasel = document.querySelector('#belegungBasel');
-    const labelsDoughnut = ['Freie Parkplätze', 'Besetzte Parkplätze'];
-    const doughnutData = {
-        labels: labelsDoughnut,
-        datasets: [{
-            label: 'Parkhausbelegung',
-            data: [free_spaces, occupied_spaces],
-            backgroundColor: [
-                'rgb(93, 45, 154)',
-                'rgb(227, 220, 236)'
-            ]
-        }]
-    };
+        const free_spaces = neuesteDaten['free_spaces'];
+        const total_spaces = neuesteDaten['total_spaces'];
+        const occupied_spaces = total_spaces - free_spaces;
 
-    new Chart(belegungBasel, {
-        type: 'doughnut',
-        data: doughnutData,
-        options: {}
-    });
+        // console.log('Freie Plätze:', free_spaces);
+        // console.log('Besetzte Plätze:', occupied_spaces);
 
-    // Liniendiagramm erstellen
-    const vorhersageBasel = document.querySelector('#vorhersageDiagramm');
-    let durchschnittsFreiePlaetze = berechneDurchschnittFreiePlaetze(parkhausCityDaten);
+         // Benutzerdefiniertes Icon für den Marker
+         var customIcon = L.icon({
+            iconUrl: 'Bilder/Stecknadel.png', // Pfad zum Icon-Bild
+            iconSize: [28.5, 38], // Größe des Icons
+            iconAnchor: [12, 41], // Ankerpunkt des Icons
+            popupAnchor: [1, -34] // Ankerpunkt des Popups
+        });
 
-    const labelsLine = [...Array(24).keys()].map(stunde => `${stunde < 10 ? '0' : ''}${stunde}:00`);
-    const lineData = {
-        labels: labelsLine,
-        datasets: [{
-            label: 'Durchschnittlich freie Plätze',
-            data: durchschnittsFreiePlaetze,
-            borderColor: 'rgb(93, 45, 154)',
-            tension: 0.1
-        }]
-    };
+        // Marker für 'Parkhaus City' setzen
+        let popupDiv = document.createElement("div");
+        let neuDat = document.createElement("b");
+        neuDat.innerHTML = `<b>${neuesteDaten.name}</b><br>Freie Plätze: ${free_spaces}<br>Total Plätze: ${total_spaces}<br>`;
+        popupDiv.appendChild(neuDat);
+        let doghnutCanvas = document.createElement("canvas");
+        let linechartCanvas = document.createElement("canvas");
+        popupDiv.appendChild(doghnutCanvas);
+        popupDiv.appendChild(linechartCanvas);
+        popupDiv.style.width = '300px';
+        
 
-    new Chart(vorhersageBasel, {
-        type: 'line',
-        data: lineData,
-        options: {
-            scales: {
-                x: {
-                    ticks: {
-                        autoSkip: true,
-                        maxTicksLimit: 12
-                    }
-                },
-                y: {
-                    beginAtZero: true,
-                    suggestedMax: total_spaces,
-                    ticks: {
-                        stepSizes: 100
+        // Doughnut-Diagramm erstellen
+        const labelsDoughnut = ['Freie Parkplätze', 'Besetzte Parkplätze'];
+        const doughnutData = {
+            labels: labelsDoughnut,
+            datasets: [{
+                label: 'Parkhausbelegung',
+                data: [free_spaces, occupied_spaces],
+                backgroundColor: [
+                    'rgb(93, 45, 154)',
+                    'rgb(227, 220, 236)'
+                ]
+            }]
+        };
+
+        doughnutChart = new Chart(doghnutCanvas, {
+            type: 'doughnut',
+            data: doughnutData,
+            options: {}
+        });
+
+        // Liniendiagramm erstellen
+        const vorhersageBasel = document.querySelector('#vorhersageDiagramm');
+        let durchschnittsFreiePlaetze = berechneDurchschnittFreiePlaetze(parkhausCityDaten);
+
+        const labelsLine = [...Array(24).keys()].map(stunde => `${stunde < 10 ? '0' : ''}${stunde}:00`);
+        const lineData = {
+            labels: labelsLine,
+            datasets: [{
+                label: 'Durchschnittlich freie Plätze',
+                data: durchschnittsFreiePlaetze,
+                borderColor: 'rgb(93, 45, 154)',
+                tension: 0.1
+            }]
+        };
+
+        lineChart =  new Chart(linechartCanvas, {
+            type: 'line',
+            data: lineData,
+            options: {
+                scales: {
+                    x: {
+                        ticks: {
+                            autoSkip: true,
+                            maxTicksLimit: 12
+                        }
+                    },
+                    y: {
+                        beginAtZero: true,
+                        suggestedMax: total_spaces,
+                        ticks: {
+                            stepSizes: 100
+                        }
                     }
                 }
             }
+        });
+        lineChartArray.push(lineChart);
+
+
+   
+
+        if (neuesteDaten.latitude && neuesteDaten.longitude) {
+            L.marker([parseFloat( neuesteDaten.latitude), parseFloat(neuesteDaten.longitude)],
+                {icon: customIcon})
+                .addTo(map).bindPopup(popupDiv);
         }
-    });
 
-    // Leaflet-Karte
-    var map = L.map('map').setView([47.5596, 7.5886], 13);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        minZoom: 8,
-        attribution: '© OpenStreetMap'
-    }).addTo(map);
+        
+        fillLineChart(lineChart, parkhausCityDaten, wti) 
 
 
-    // Benutzerdefiniertes Icon für den Marker
-var customIcon = L.icon({
-  iconUrl: 'Bilder/Stecknadel.png', // Pfad zum Icon-Bild
-  iconSize: [28.5, 38], // Größe des Icons
-  iconAnchor: [12, 41], // Ankerpunkt des Icons
-  popupAnchor: [1, -34] // Ankerpunkt des Popups
-});
-
-    // Marker für 'Parkhaus City' setzen
-    if (neuesteDaten.latitude && neuesteDaten.longitude) {
-      L.marker([parseFloat(neuesteDaten.latitude), parseFloat(neuesteDaten.longitude)], {icon: customIcon}).addTo(map)
-            .bindPopup(`<b>${neuesteDaten.name}</b><br>Freie Plätze: ${free_spaces}<br>Total Plätze: ${total_spaces}<br> <div>
-            <canvas id="vorhersageDiagramm"></canvas>
-            </div>`);
-    }
+    };
 
 
 
 
-    
+/*
+    fillLineChart(wti);
+*/
+
 }
 
 function berechneDurchschnittFreiePlaetze(parkhausCityDaten, wochentagIndex) {
@@ -125,10 +165,8 @@ function berechneDurchschnittFreiePlaetze(parkhausCityDaten, wochentagIndex) {
 
     // Nur Daten des ausgewählten Wochentags herausfiltern
     let wochentagsDaten = sortierteDaten.filter(datum => new Date(datum.published).getDay() === wochentagIndex);
-
     // Nur die Daten der letzten vier ausgewählten Wochentage nehmen
-    let letzteVierWochentage = wochentagsDaten.slice(-4);
-
+    let letzteVierWochentage = wochentagsDaten.slice(-(1*24*4)); //letzte 5 Wochen -> 5*24*4
     // Stundenweise Datenstruktur vorbereiten
     let stundenDaten = new Array(24).fill(0).map(() => []);
 
@@ -145,14 +183,28 @@ function berechneDurchschnittFreiePlaetze(parkhausCityDaten, wochentagIndex) {
         let durchschnitt = stundenListe.reduce((sum, curr) => sum + curr, 0) / stundenListe.length;
         return Math.ceil(durchschnitt);
     });
-}
 
-document.getElementById('wochentagAuswahl').addEventListener('change', async (event) => {
+}
+wochenTagAuswahl.addEventListener('change', async (event) => {
     let wochentagIndex = parseInt(event.target.value);
-    let parkhausCityDaten = await fetchData(url); // Daten neu laden
-    let durchschnittsFreiePlaetze = berechneDurchschnittFreiePlaetze(parkhausCityDaten['Parkhaus City'], wochentagIndex);
-    // Aktualisieren Sie hier Ihre Diagramme oder andere UI-Elemente mit den neuen Daten
+    let i = 0;
+    for (const [key, value] of Object.entries(parkhausDaten)) {
+        let durchschnittsFreiePlaetze = berechneDurchschnittFreiePlaetze(value, wochentagIndex);
+        lineChartArray[i].data.datasets[0].data = durchschnittsFreiePlaetze;
+        lineChartArray[i].update();
+        i++;
+    }
+
 });
+
+function fillLineChart(chart, pDaten, dayIndex) {
+
+    let durchschnittsFreiePlaetze = berechneDurchschnittFreiePlaetze(pDaten, dayIndex);
+
+    // Aktualisieren des Liniendiagramms
+    chart.data.datasets[0].data = durchschnittsFreiePlaetze;
+    chart.update();
+}
 
 
 init();
